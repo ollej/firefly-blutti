d7e26810409d037d53f5a065a743a28373b8a683
Olle Wreede <olle@wreede.se>
2024-12-24 13:34:24 +0100

    Update tile collision code

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 use core::cell::OnceCell;
 use firefly_rust::*;
+use fixedstr::{str256, str_format};
 
 const TILE_WIDTH: u8 = 8;
 const TILE_HEIGHT: u8 = 8;
@@ -63,8 +64,8 @@ impl Default for Blutti {
     fn default() -> Self {
         Self {
             position: Point {
-                x: 120 - Self::SIZE / 2,
-                y: 160 - Self::SIZE - 8,
+                x: 120 - Self::SIZE,
+                y: 160 - Self::SIZE - TILE_HEIGHT as i32,
             },
             jump_timer: 0,
             dash_timer: 0,
@@ -82,7 +83,7 @@ impl Blutti {
     const DASH_TIME: i32 = 8;
     const DASH_WAIT_TIME: i32 = 32;
     const DASH_SPEED: i32 = 1;
-    const GRAVITY: i32 = 2;
+    const GRAVITY: i32 = 1;
     const MIN: Point = Point::MIN;
     const MAX: Point = Point {
         x: Point::MAX.x - Self::SIZE,
@@ -112,16 +113,12 @@ impl Blutti {
 
     fn move_left(&mut self) {
         self.direction = Direction::Left;
-        if self.standing() {
-            self.movement = -Self::SPEED;
-        }
+        self.movement = -Self::SPEED;
     }
 
     fn move_right(&mut self) {
         self.direction = Direction::Right;
-        if self.standing() {
-            self.movement = Self::SPEED;
-        }
+        self.movement = Self::SPEED;
     }
 
     fn start_jump(&mut self) {
@@ -137,58 +134,87 @@ impl Blutti {
     }
 
     fn movement(&mut self) {
+        let mut new_x = self.position.x + self.movement;
+        let mut new_y = self.position.y;
+
+        if self.dash_timer > 0 {
+            new_x = self.position.x + Self::DASH_SPEED * self.movement;
+        }
         if self.jump_timer > 0 {
-            self.position = Point {
-                x: self.position.x,
-                y: self.position.y - Self::JUMP_SPEED,
-            };
+            new_y -= Self::JUMP_SPEED;
             self.jump_timer -= 1;
         }
-        if self.dash_timer > 0 {
-            self.position = Point {
-                x: self.position.x + Self::DASH_SPEED * self.movement,
-                y: self.position.y,
-            };
-            if self.dash_timer == 1 {
-                self.dash_timer = -Self::DASH_WAIT_TIME;
-            } else {
-                self.dash_timer -= 1;
-            }
-        }
-        if self.dash_timer < 0 {
-            self.dash_timer += 1;
-        }
-        if self.standing() {
-            if self.movement != 0 {
-                self.position = Point {
-                    x: self.position.x + self.movement,
-                    y: self.position.y,
-                };
-                self.movement = 0;
-            }
-        } else {
-            if self.jump_timer == 0 && self.dash_timer <= 0 {
-                self.position = Point {
-                    x: self.position.x,
-                    y: self.position.y + Self::GRAVITY,
-                }
-            }
+        if self.jump_timer == 0 && self.dash_timer <= 0 {
+            new_y += Self::GRAVITY;
         }
 
-        self.position.x = self.position.x.clamp(Self::MIN.x, Self::MAX.x);
-        self.position.y = self.position.y.clamp(Self::MIN.y, Self::MAX.y);
+        if self.dash_timer == 1 {
+            self.dash_timer = -Self::DASH_WAIT_TIME;
+        } else if self.dash_timer > 1 {
+            self.dash_timer -= 1;
+        } else if self.dash_timer < 0 {
+            self.dash_timer += 1;
+        }
+        if self.movement != 0 {
+            self.movement = 0;
+        }
+
+        let new_position = Point {
+            x: new_x.clamp(Self::MIN.x, Self::MAX.x),
+            y: new_y.clamp(Self::MIN.y, Self::MAX.y),
+        };
+
+        if self.position.x != new_position.x {
+            let test_x = if new_position.x > self.position.x {
+                new_position.x + TILE_WIDTH as i32 - 1
+            } else {
+                new_position.x
+            };
+            if self.is_tile_empty(Point {
+                x: test_x,
+                y: self.position.y,
+            }) {
+                self.position.x = new_position.x;
+            }
+        }
+        if self.position.y != new_position.y {
+            let test_y = if new_position.y > self.position.y {
+                new_position.y + TILE_HEIGHT as i32 - 1
+            } else {
+                new_position.y
+            };
+            if self.is_tile_empty(Point {
+                x: self.position.x,
+                y: test_y,
+            }) {
+                self.position.y = new_position.y;
+            }
+        }
+    }
+
+    fn is_tile_empty(&self, test_point: Point) -> bool {
+        //log_debug(str_format!(str256, "x: {}", test_point.x).as_str());
+        //log_debug(str_format!(str256, "y: {}", test_point.y).as_str());
+        let tile_pos = get_tile_index(test_point);
+        //log_debug(str_format!(str256, "tile_pos: {}", tile_pos).as_str());
+        let level_pos = LEVEL[tile_pos as usize];
+        //log_debug(str_format!(str256, "level_pos: {}", level_pos).as_str());
+        level_pos == 0
     }
 
     fn standing(&self) -> bool {
-        let tile_pos = get_tile_index(self.position);
-        let pos = LEVEL[tile_pos as usize];
-        pos != 0
+        let position_below = Point {
+            x: self.position.x,
+            y: self.position.y + TILE_HEIGHT as i32,
+        };
+        !self.is_tile_empty(position_below)
     }
 }
 
 fn get_tile_index(point: Point) -> usize {
     let tile_x = point.x / TILE_WIDTH as i32;
-    let tile_y = point.y / TILE_WIDTH as i32 + 1;
+    let tile_y = point.y / TILE_WIDTH as i32;
+    //log_debug(str_format!(str256, "tile_x: {} tile_y: {}", tile_x, tile_y).as_str());
     (tile_y * TILES_H as i32 + tile_x) as usize
 }
 
