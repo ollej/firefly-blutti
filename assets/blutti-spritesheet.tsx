5f0e4e95abc51e4236a7816109520872fa97a467
Olle Wreede <olle@wreede.se>
2025-01-04 23:06:50 +0100

    Resize spritesheet to 16x16 sprites

--- assets/blutti-spritesheet.tsx
+++ assets/blutti-spritesheet.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.11" tiledversion="1.11.0" name="Blutti" tilewidth="8" tileheight="8" tilecount="64" columns="8" objectalignment="topleft">
- <image source="spritesheet.png" width="64" height="64"/>
- <tile id="13">
+<tileset version="1.11" tiledversion="1.11.0" name="Blutti" tilewidth="8" tileheight="8" tilecount="256" columns="16" objectalignment="topleft">
+ <image source="spritesheet.png" width="128" height="128"/>
+ <tile id="21">
   <properties>
    <property name="movement" type="int" value="0"/>
   </properties>
@@ -12,11 +12,11 @@
    <wangtile tileid="3" wangid="1,1,1,1,1,1,1,1"/>
    <wangtile tileid="4" wangid="1,1,1,1,1,1,1,1"/>
    <wangtile tileid="5" wangid="1,1,1,1,1,1,1,1"/>
-   <wangtile tileid="12" wangid="1,1,1,1,1,1,1,1"/>
-   <wangtile tileid="13" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="20" wangid="1,1,1,1,1,1,1,1"/>
    <wangtile tileid="21" wangid="1,1,1,1,1,1,1,1"/>
-   <wangtile tileid="22" wangid="1,1,1,1,1,1,1,1"/>
-   <wangtile tileid="30" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="37" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="38" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="54" wangid="1,1,1,1,1,1,1,1"/>
   </wangset>
  </wangsets>
 </tileset>
