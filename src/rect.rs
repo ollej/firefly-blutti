10f9bf14c431fc4bdd1b428e07514fc46219a958
Olle Wreede <olle@wreede.se>
2026-01-04 17:57:01 +0100

    Store width and height in Rect

--- src/rect.rs
+++ src/rect.rs
@@ -4,29 +4,53 @@ use crate::constants::*;
 
 #[derive(Copy, Clone, PartialEq, Default, Debug)]
 pub struct Rect {
-    top_left: Point,
-    bottom_right: Point,
+    position: Point,
+    width: i32,
+    height: i32,
 }
 
 impl Rect {
     pub fn from(position: Point) -> Self {
         Self {
-            top_left: position,
-            bottom_right: Point {
-                x: position.x + TILE_WIDTH - 1,
-                y: position.y + TILE_HEIGHT - 1,
-            },
+            position,
+            width: TILE_WIDTH,
+            height: TILE_HEIGHT,
+        }
+    }
+
+    pub fn top_left(&self) -> Point {
+        self.position
+    }
+
+    pub fn top_right(&self) -> Point {
+        Point {
+            x: self.position.x + self.width - 1,
+            y: self.position.y,
+        }
+    }
+
+    pub fn bottom_right(&self) -> Point {
+        Point {
+            x: self.position.x + self.width - 1,
+            y: self.position.y + self.height - 1,
+        }
+    }
+
+    pub fn bottom_left(&self) -> Point {
+        Point {
+            x: self.position.x,
+            y: self.position.y + self.height - 1,
         }
     }
 
     pub fn contains(&self, point: Point) -> bool {
-        point.x >= self.top_left.x
-            && point.x <= self.bottom_right.x
-            && point.y >= self.top_left.y
-            && point.y <= self.bottom_right.y
+        point.x >= self.position.x
+            && point.x <= self.bottom_right().x
+            && point.y >= self.position.y
+            && point.y <= self.bottom_right().y
     }
 
     pub fn overlaps(&self, other: Rect) -> bool {
-        self.contains(other.top_left) || self.contains(other.bottom_right)
+        self.contains(other.position) || self.contains(other.bottom_right())
     }
 }
